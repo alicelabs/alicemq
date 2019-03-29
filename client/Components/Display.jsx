e50b9b9267c337bca15165f4102c5f60fd1fe032
Chris Air <sdchrisn@gmail.com>
2019-03-29 15:39:55 -0700

    D3 works within electron currently only in v3

--- client/Components/Display.jsx
+++ client/Components/Display.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Chart from './D3/Chart.jsx';
 
 const Display = (props) => {
   
   return (
     <div className="viz"> 
-    <p>viz</p>
+      <p>viz</p>
+      <Chart />
     </div>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: path.join('client/dist')
+        contentBase: './'
     },
     module: {
         rules: [
