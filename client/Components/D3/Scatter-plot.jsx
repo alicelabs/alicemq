19548c7a3f2cb14be51604e071ec7b53482f62a2
Chris Air <sdchrisn@gmail.com>
2019-03-29 17:02:40 -0700

    Electron now works with D3 v4+, and removed storing bundles in repo

--- client/Components/D3/Scatter-plot.jsx
+++ client/Components/D3/Scatter-plot.jsx
@@ -1,6 +1,6 @@
 // unfinished/src/components/scatter-plot.jsx
 import React        from 'react';
-import d3           from 'd3';
+import * as d3      from 'd3';
 import DataCircles  from './Data-circles.jsx';
 import XYAxis       from './X-y-axis.jsx';
 
@@ -13,14 +13,14 @@ const yMax   = (data)  => d3.max(data, (d) => d[1]);
 
 // Returns a function that "scales" X coordinates from the data to fit the chart
 const xScale = (props) => {
-  return d3.scale.linear()
+  return d3.scaleLinear()
     .domain([0, xMax(props.data)])
     .range([props.padding, props.width - props.padding * 2]);
 };
 
 // Returns a function that "scales" Y coordinates from the data to fit the chart
 const yScale = (props) => {
-  return d3.scale.linear()
+  return d3.scaleLinear()
     .domain([0, yMax(props.data)])
     .range([props.height - props.padding, props.padding]);
 };
@@ -29,6 +29,6 @@ export default (props) => {
   const scales = { xScale: xScale(props), yScale: yScale(props) };
   return <svg width={props.width} height={props.height}>
     <DataCircles {...props} {...scales} />
-    <XYAxis {...props} {...scales} />
+    {/* <XYAxis {...props} {...scales} /> */}
   </svg>
 }
--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-
 import css from './static/style.css';
 
 const el = document.getElementById('app');
