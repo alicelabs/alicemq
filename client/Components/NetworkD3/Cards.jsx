20dac24ec608d609745003498d8a988ff7628592
yudataguy <yudataguy@gmail.com>
2019-04-03 10:30:41 -0700

    create card component

--- client/Components/NetworkD3/Cards.jsx
+++ client/Components/NetworkD3/Cards.jsx
@@ -3,3 +3,9 @@ import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
+function InfoCards(props) {
+  <div className='info-cards'>
+    <Card />
+  </div>
+}
